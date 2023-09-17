0be10cb8b3f947b241c2735af22e812696e37877
Judy <judychue@pursuit.org>
2023-09-16 23:38:08 -0400

    added notes

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const findMostCommonItem = (arr) => {
             winnerSoFarName = key
         }
     }
+    console.log(frequencyMap)
     return winnerSoFarName
  }
 
@@ -45,13 +46,27 @@ findMostCommonItem(fruits)
 // key= a, value = 1
 // a: 1
 
-// Hash map allows us to use an object in js - it is a data structure. Any coding language can implement a hash map, in js we use the {} (or object) 
+// note: line 21-  let winnerSoFar = 0
+// "0" is actually not the lowest number, but for this ex "0" is fine. We could add -infinity which = the lowest number.
+
+// Hash map (hash table, a hash)allows us to use an object in js - it is a data structure. It is used to count values or keep track of them. It is an object with key value pairs. Normally are numbers or booleans or sometimes just true. (counting up).
+
+// Hash maps are very efficient:
+{apple: 1, banana: 3, pear: 3, lime: 1, lemon: 1}
+apple: 1
+banana: 3
+lemon: 1
+lime: 1
+pear: 3
+
+// Any coding language can implement a hash map, in js we use the {} (or object) 
 
 // This ex gives us the frequency that the fruits appear on the list
 
-
-
 /*
 APPLE: 1
-BANANA: 1 1
+BANANA: 3
+PEAR: 3
+LIME: 1
+LEMON: 1
 */ 
